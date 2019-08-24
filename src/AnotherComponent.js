12336f615de812f8b0ebb42fcbe3a5fc7a6cdba1
faisal arkan <faisalarkan21@gmail.com>
2019-08-24 10:03:39 +0700

    init changes

---
+++ src/AnotherComponent.js
@@ -0,0 +1,30 @@
+import { Button } from "antd";
+import React from "react";
+
+class AnotherComponent extends React.Component {
+  render() {
+
+    console.log('this.props', this.props)
+    return (
+      <div style={{backgroundColor: 'red'}}>
+        <Button
+          onClick={this.props.handleIncrementNumber}
+          className="coba"
+          type="primary"
+        >
+          Tambah
+        </Button>
+        <Button
+          onClick={this.props.handleIncrementNumber}
+          className="coba"
+          type="primary"
+        >
+          Kurang
+        </Button>
+        {this.props.number}
+      </div>
+    );
+  }
+}
+
+export { AnotherComponent };
--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import MaskedInput from "react-text-mask";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import "./App.css";
+import { Button, Input } from "antd";
+import { AnotherComponent } from "./AnotherComponent";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+      numberMultiply: 0,
+      numberMultiply1: 0,
+      numberMultiply2: 0
+    };
+  }
+
+  handleIncrementNumber = () => {
+    this.setState({
+      number: this.state.number + 1
+    });
+  };
+
+  handleDecrementNumber = () => {
+    this.setState({
+      number: this.state.number - 1
+    });
+  };
+
+  handleMultiplyNumber = () => {
+    this.setState({
+      number: this.state.number * this.state.numberMultiply
+    });
+  };
+
+  handleChange = e => {
+    console.log(e.target.value);
+    this.setState({ numberMultiply: e.target.value });
+  };
+
+  render() {
+    const { number: mb, numberMultiply } = this.state;
+    return (
+      <div className="App"  style={{backgroundColor: 'blue'}}>
+        <AnotherComponent
+         {...this.state}
+          handleIncrementNumber={this.handleIncrementNumber}
+          handleDecrementNumber={this.handleDecrementNumber}
+        />
+        <Button
+          onClick={this.handleMultiplyNumber}
+          className="coba"
+          type="primary"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Kali
+        </Button>
+        <br />
+        {/* <Input type="number" style={{width: 100}} value={numberMultiply} onChange={this.handleChange} ></Input> */}
+        <MaskedInput
+          mask={s => Array.from(s).map(() => /[0-9]/i)}
+          guide={false}
+          class="input"
+          id="name"
+          placeHolder="input multiplier"
+          type="number"
+          value={this.state.numberMultiply}
+          onChange={this.handleChange}
+        />
+
+        <br />
+        {/* {mb} */}
+      </div>
+    );
+  }
 }
 
 export default App;
